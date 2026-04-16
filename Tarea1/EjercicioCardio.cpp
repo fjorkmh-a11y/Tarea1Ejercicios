964ad6c4f2c06ecff0f882b8f5a34f97ebf1ad58
fjorkmh-a11y <fjork.mh@gmail.com>
2026-04-16 17:18:35 -0400

    Version final

--- Tarea1/EjercicioCardio.cpp
+++ Tarea1/EjercicioCardio.cpp
@@ -1,10 +1,12 @@
 #include "EjercicioCardio.h"
 
-//Lista de inicializacion para llamar clase padre
+//Implementacion de constructor. Destructor. Get y set. Mostrarinfo()
+
+//Constructor para llamar clase padre
 EjercicioCardio::EjercicioCardio(int codigo, string nombre, string tipo, string intensidad, int tiempo, string descripcion, int ultimaSemana, string implemento)
 
     //Llamar a clase padre
-    : Ejercicio(codigo, nombre, tipo, intensidad, tiempo, descripcion, ultimaSemana){
+    :Ejercicio(codigo, nombre, tipo, intensidad, tiempo, descripcion, ultimaSemana){
         this->implemento = implemento; //Atributo propio
     }
 
@@ -16,16 +18,13 @@ string EjercicioCardio::getImplemento() {
     return implemento;
 }
 
-//PARAMETRO// recibe, no regresa
 void EjercicioCardio::setImplemento(string implemento){
     this-> implemento = implemento; //Parametro coincide con atributo: this->   
 }
 
-//Mostrar info general mas la propia de cardio
+//Polimorfismo para mostrar info general mas la propia de cardio
 void EjercicioCardio::mostrarInfo(){
-    //Llamar al metodo del padre para mostrar la info general basica
     Ejercicio::mostrarInfo();
-
     //Detalles propios de cardio
     cout << "Tipo: Cardiovascular" << endl;
     cout << "Implemento necesario: " << implemento << endl;
